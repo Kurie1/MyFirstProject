a5715265542f1cba89609336492ac9c9a40fca2e
Kurie <flykuro211@gmail.com>
2022-04-08 19:58:33 +0700

    bai 2

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -4,10 +4,43 @@ using namespace std;
 
 int main() {
 
+	int a, b, c;
+	
+	cout << "nhap canh a:";
 
+	cin >> a;
+	
+	cout << "nhap canh b:";
 
+	cin >> b;
 
+	cout << "nhap canh c:";
 
+	cin >> c;
+
+	if (a + b > c && a + c > b && b + c > a) {
+
+		cout << "tam giac hop le"<<endl;
+
+		if (a == b || a == c || b == c) {
+			
+			cout << "tam giac can" << endl;
+		}
+		else {
+			cout << "tam giac ko can" << endl;
+		}
+		
+		if (a == b&&a==c) {
+
+			cout << "tam giac deu" << endl;
+		}
+		else {
+			cout << "tam giac ko deu" << endl;
+		}
+	}
+	else {
+		cout << "ko phai tam giac";
+	}
 
 	cout << endl;
 
