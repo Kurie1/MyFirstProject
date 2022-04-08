c2739082ec5e869c2cb3b66d2e750897c4256cac
Kurie <flykuro211@gmail.com>
2022-04-08 21:42:58 +0700

    so chia het cho 30

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
 
 
-	
+	for (int i = 0; i <= 30; i++) {
 
-	for (int i = 0; i <=100; i=i+2) {
+		if (i % 3 == 0) {
 
-
-		cout << i << endl;
-
-	}
-	for (int i = 1; i <= 100; i = i +2 ) {
-
-
-		cout << i << endl;
+			cout << i << endl;
+		}
 
 	}
+
 
 	cout << endl;
 
 	system("pause");
 
 	return 0;
-
 }
